fce3bc2321c20b3774964b74bf7fd01ab992c6ca
Choi <456tjdrb@gmail.com>
2026-06-09 20:52:22 +0900

    refactor: 전투 객체 참조 구조 개선 및 턴 관리 추가

--- Pneuma-Client/Assets/Scripts/Battle/BattleManager.cs
+++ Pneuma-Client/Assets/Scripts/Battle/BattleManager.cs
@@ -6,6 +6,12 @@ public class BattleManager : MonoBehaviour
 {
     public static BattleManager Instance { get; private set; }
     public BattleState CurrentState { get; private set; } = BattleState.None;
+    public int CurrentTurn { get; private set; } = 0;
+
+    // TODO: Player, Enemy 구현 후 참조 연결
+    //public Player CurrentPlayer { get; private set; }
+    //public List<Enemy> Enemies { get; private set; }
+
     public event Action<BattleState> OnBattleStateChanged;
 
     private void Awake()
@@ -21,8 +27,16 @@ public class BattleManager : MonoBehaviour
 
     private void Start()
     {
+
         ChangeState(BattleState.Start); 
     }
+
+    // TODO: Player, Enemy 구현 후 참조 연결
+    // private void Initialize(Player player, List<Enemy> enemies)
+    // {
+    //     CurrentPlayer = player;
+    //     Enemies = enemies;
+    // }
 
     // 상태 전환 메서드
     public void ChangeState(BattleState newState)
@@ -31,9 +45,15 @@ public class BattleManager : MonoBehaviour
 
         CurrentState = newState;
 
-        Debug.Log($"Battle State Changed : {CurrentState}");
+        Debug.Log($"[BattleManager] State Changed : {CurrentState}");
 
         OnBattleStateChanged?.Invoke(CurrentState);
+    }
+
+    public void IncreaseTurn()
+    {
+        // 플레이어 턴에 턴 수 증가
+        CurrentTurn++;
     }
 
     public void CheckBattleResult()
@@ -42,17 +62,23 @@ public class BattleManager : MonoBehaviour
         // 호출할 때마다 계속 전달해야하므로 
         // BattleManager에서 Player와 Enemy의 체력을 직접 참조하여 승패를 판단하도록 변경
 
-        // TODO: Player와 EnemyManager가 추가된 후 아래 코드 활성화
-        
-        // if(Player.Instance.CurrentHp <= 0)
+        // TODO: Player, Enemy 구현 후 참조 연결
+        // if(CurrentPlayer.CurrentHp <= 0)
         // {
         //     ChangeState(BattleState.Defeat);
         //     return;
         // }
-
-        // if(EnemyManager.Instance.IsAllDead())
+        // if(Enemies.All(enemy => enemy.IsDead))
         // {
         //     ChangeState(BattleState.Victory);
         // }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
     }
 }
