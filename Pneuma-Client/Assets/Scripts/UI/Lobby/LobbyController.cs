d2d1960b1e10df7a5e1721d93fc830564ef17e65
Choi <456tjdrb@gmail.com>
2026-06-15 12:39:36 +0900

    feat: 기본 Lobby UI 정의

---
+++ Pneuma-Client/Assets/Scripts/UI/Lobby/LobbyController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyController : MonoBehaviour
+{
+    private const string BattleScene = "SampleScene";
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene(BattleScene);
+    }
+
+    public void OnClickCollection()
+    {
+        // 도감 씬 이동
+        Debug.Log("도감 씬으로 이동");
+    }
+
+    public void OnClickSettings()
+    {
+        // 설정 창 열기
+        Debug.Log("세팅 UI 활성화");
+    }
+
+    public void OnClickExit()
+    {
+        Debug.Log("앱 종료");
+        Application.Quit();
+    }
+}
